036d301a828674fb503d706322aa17a060192073
Khuong Nguyen <thekhuong3991@gmail.com>
2023-10-02 07:03:23 -0700

    new branch and reseeded

--- backend/db/seeders/20230903173827-demo-review.js
+++ backend/db/seeders/20230903173827-demo-review.js
@@ -14,28 +14,28 @@ module.exports = {
         {
           spotId: 2,
           userId: 2,
-          review: "Cold",
+          review: "Cold and I like it",
           stars: 4,
         },
         {
           spotId: 1,
           userId: 4,
           review:
-            "It is a beautiful house! It had all the space we needed and then some. The beds were comfortable and bathrooms really nice. My kids loved all of the games. The view is unbeatable! The house has everything you need, which is so nice. The hosts proactively communicated multiple times and told us everything we need to know. Highly recommend!",
+            "It is a beautiful house! It had all the space we needed and then some. This is a fantastic place.",
           stars: 5,
         },
         {
           spotId: 1,
           userId: 3,
           review:
-            "The weather is excessively chilly and windy, and the floor is quite noisy.",
+            "The weather is excessively chilly and windy, and the floor is quite noisy.  This is a fantastic place.",
           stars: 3,
         },
         {
           spotId: 1,
           userId: 1,
           review:
-            "This place was a dream! Everything was exactly as pictured and as described. One the cleanest airbnbs we have ever had and definitely recommend. We had a blast. Traci and Benjamin were great hosts",
+            "This place was a dream! Everything was exactly as pictured and as described.  This is a fantastic place.",
           stars: 5,
         },
       ],
